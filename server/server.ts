e0ebc57f0c032ff02d3187880c14b1e83badf28d
Maia <Maiakbard@gmail.com>
2026-05-03 22:38:15 -0500

    Add start script to package.json, Add test express server/route to server.ts file, create .env file and add it to .gitingnore file

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,13 @@
 import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+// routes
+app.get('/test', (req, res) => {
+  res.send("YAYA")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`)
+})
