368df5c8013043a3989dbec0119901d2fffcc94f
Maia <Maiakbard@gmail.com>
2026-05-03 23:36:35 -0500

    Set up Express server with MongoDB connection

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,18 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
 const PORT = 3000
 
+app.use(express.json())
+// database connection
+try {
+  await mongoose.connect(process.env.MONGODB_URI!)
+  console.log('Connected to MongoDB!')
+} catch( err ) {
+  console.log(err)
+}
 // routes
 app.get('/test', (req, res) => {
   res.send("YAYA")
