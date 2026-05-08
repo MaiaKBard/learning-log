68673181a541105f04d86daf7cb93e88eb04bb0e
Maia <Maiakbard@gmail.com>
2026-05-08 16:14:36 -0500

    Add try/catch block in AIResponse and a edge protection if there is no text

--- server/utils/ai.ts
+++ server/utils/ai.ts
@@ -1,15 +1,20 @@
 import { GoogleGenAI } from "@google/genai"
 
   const AIResponse = async(text) => {
-  const limitedText = text.slice(0, 500)
-  const client = new GoogleGenAI( {apiKey: process.env.GEMINI_API_KEY})
+  try {
+    if (!text) return
+    const limitedText = text.slice(0, 500)
+    const client = new GoogleGenAI( {apiKey: process.env.GEMINI_API_KEY})
 
-  const response = await client.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents:`Create a breakdown and summary of content: ${limitedText}`
-  })
+    const response = await client.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents:`Create a breakdown and summary of content: ${limitedText}`
+    })
 
-  return response.text
+    return response.text
+ } catch(err) {
+  console.log(err)
+ }
 }
 
 export default AIResponse
