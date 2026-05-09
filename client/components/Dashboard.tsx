8cc9154c9cfc5e6ac3e4d0a1b3577152c3d2efb2
Maia <Maiakbard@gmail.com>
2026-05-09 12:16:15 -0500

    Add functioning entry view - clickable cards in dashbaord

--- client/components/Dashboard.tsx
+++ client/components/Dashboard.tsx
@@ -4,6 +4,14 @@ import { useState, useEffect} from 'react'
 const Dashboard = () => {
   const [data, setData ] = useState([])
   const [loading, setLoading ] = useState(true)
+  const [selectedEntry, setSelectedEntry] = useState<{
+    _id: string,
+    url: string,
+    title: string,
+    summary: string,
+    breakdown: string,
+    deeperDive: string
+  } | null>(null)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,15 +36,38 @@ const Dashboard = () => {
         <p className="page-label">DASHBOARD PAGE</p>
         <h1>Learning Dashboard</h1>
         <p>Your saved resources</p>
-        {loading ? <p>Loading...</p> :
-          <div className="card-grid"> {
-            data.map(({ _id, title, summary }) => (
-              <div key={_id} className="card"> 
-                <div className="card-title">{title}</div>
-                <div className="card-summary">{summary}</div>
+        {selectedEntry === null ? (
+          loading ? <p>Loading...</p> :
+            <div className="card-grid"> {
+              data.map(({ _id, url, title, summary, breakdown, deeperDive }) => (
+                <div key={_id} className="card" onClick={() => setSelectedEntry({_id, url, title, summary, breakdown, deeperDive})}> 
+                  <div className="card-title">{title}</div>
+                  <div className="card-summary">{summary}</div>
+                </div>
+              ))}
+            </div>
+        ) : (
+            <div className="entry-view">
+              <button onClick={() => setSelectedEntry(null)}> ← </button>
+              <h3>{selectedEntry.title}</h3>
+              <div>
+                <h2>URL</h2>
+                <p>{selectedEntry.url}</p>
               </div>
-            ))
-          }</div>
+              <div>
+                <h2>Deeper Dive</h2>
+                <p>{selectedEntry.deeperDive}</p>
+              </div>
+              <div>
+                <h2>Summary</h2>
+                <p>{selectedEntry.summary}</p>
+              </div>
+              <div>
+                <h2>Breakdown</h2>
+                <p>{selectedEntry.breakdown}</p>
+              </div>
+            </div>
+          )
         }
       </div>
     </>
